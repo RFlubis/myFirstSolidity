8539defda2c0337fb1449ab053d24980d288421e
RFlubis <romziflubis@gmail.com>
2025-12-17 13:03:45 +0700

    initialCommit

---
+++ SimpleStorage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19; // stating version
+
+contract SimpleStorage{
+    bool hasFavoriteNumber= true;
+    uint favoriteNumber = 88;
+    string favoriteNumberinText = "88";
+    address myAddress = 0xf28F917882a55DDB8D5380bdb9d536d01fbe3A39;
+    bytes32 favoriteBytes32 = "fish";
+}
